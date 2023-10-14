40378c8cf0313d19baf00cbc1e4e816d2c2fd11a
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-10-14 21:16:12 +0200

    Ajout du contrôleur Home

--- migrations/Version20231014124824.php
+++ migrations/Version20231014124824.php
@@ -14,7 +14,7 @@ final class Version20231014124824 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Product table';
     }
 
     public function up(Schema $schema): void
