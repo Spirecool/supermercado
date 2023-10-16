0d20750a1aebe7930798bddde2d8b9e6bcb9a5ab
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-10-16 17:39:18 +0200

    Ajout du CRUD pour les produits

--- src/Entity/Discount.php
+++ src/Entity/Discount.php
@@ -103,4 +103,8 @@ class Discount
 
         return $this;
     }
+    public function __toString()
+    {
+        return $this->discount_percentage;
+    }
 }
---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('image_file')
+            ->add('created_at')
+            ->add('discount')
+            ->add('user')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
