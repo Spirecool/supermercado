ca0c9cb8d0f2a157dc38b53d89330d871c499b80
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-10-17 15:25:39 +0200

    Ancien prix affiché sur card quand promotion

--- src/Form/DiscountType.php
+++ src/Form/DiscountType.php
@@ -7,15 +7,37 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\LessThan;
 
 class DiscountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('discount_percentage')
-            ->add('start_date')
-            ->add('end_date')
+        ->add('discount_percentage', options:[
+            'label' => 'Montant de la promotion',
+            'constraints' => [
+                new Positive([
+                    'message' => 'La promotion ne peux pas être négative'
+                ]),
+                new LessThan([
+                    'value' => 100,
+                    'message' => 'La promotion doit être inférieure à 100%',
+                ]),
+            ]
+        ])
+            ->add('start_date', DateType::class, [
+                'label' => 'Date de début de la promotion',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('end_date', DateType::class, [
+                'label' => 'Date de fin de la promotion',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('products')
         ;
     }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class ProductType extends AbstractType
 {
@@ -14,11 +17,22 @@ class ProductType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('price')
+            ->add('price', MoneyType::class, options:[
+                'label' => 'Prix',
+                'divisor' => 100,
+                'constraints' => [
+                    new Positive(
+                        message: 'Le prix ne peut être négatif'
+                    )
+                ]
+            ])
             ->add('image_file')
-            ->add('created_at')
+            ->add('created_at', DateType::class, [
+                'label' => 'Date de début de la promotion',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('discount')
-            ->add('user')
             ->add('category')
         ;
     }
