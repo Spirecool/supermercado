b643d61e90bf84822b8b3eb1c582e79dab6ed14f
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-10-25 11:42:07 +0200

    Ajout du filtre des ctagories -> OK

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -1,21 +1,32 @@
 <?php
-
 namespace App\Controller;
 
-
 use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class CatalogueController extends AbstractController
 {
     #[Route('/catalogue', name: 'app_catalogue')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request): Response
     {
+        $category = $request->query->get('category'); 
+        $products = $productRepository->findAll();
+        $categories = $categoryRepository->findAll();
+
+        if ($category) {
+            // Si une catégorie est sélectionnée, filtrez les produits en conséquence
+            $products = $productRepository->findByCategory($category); // Supposons que vous ayez une méthode findByCategory dans ProductRepository.
+        }
+
         return $this->render('catalogue/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $products,
+            'categories' => $categories,
+         
         ]);
     }
-    
 }
